8c4887f1b0b587a3a09a987b964ea804068339c3
francisco cuesta <cuestafrancisco@gmail.com>
2020-10-06 17:41:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LinkContainer from './components/LinkContainer';
+
+function App() {
+  return (
+    <div className="App">
+     { /*responsible for showing all components  returning from the app*/}
+        <LinkContainer /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+
+class Form extends Component 
+{
+    constructor(props) 
+    {
+        super(props);
+        /* TODO - set initial state for link name and URL */
+        this.state = {  name:"", URL:"" }
+    }
+    handleChange = event => 
+    { /* TODO - Logic for changing state based on form changes */
+        this.setState ({ 
+            name: event.target.value,
+            URL: event.target.value 
+        })
+    }
+    urlChange = (event) => 
+    {
+        this.setState(
+            {
+                name: event.target.value,
+             URL: event.target.value
+            })
+    }
+    onFormSubmit = (event) => 
+    {   // function called when submits
+        // to prevent page reload on form submit
+        event.preventDefault();
+        const submission = {
+            name: this.state.name,
+            URL: this.state.URL
+          };
+        /*TODO - Logic for calling props to handle submission and setting state changes */
+        this.props.handleSubmit(submission);
+        this.setState({name: "", URL: ""})
+        
+    }
+
+    
+    render() 
+    {   return(<form onSubmit={this.onFormSubmit} >
+                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+                 < label for = "name" > Name</label>
+                 < input type = "text"  value={this.state.name} onChange={this.handleChange} />
+                 < label for  = "url" > URL</label >
+                 < input type = "text"  value={this.state.URL}
+                    onChange={this.urlChange} />
+                < button type="submit">Submit </button>
+                
+            </form>
+        )
+    
+    }
+}
+
+export default Form;
